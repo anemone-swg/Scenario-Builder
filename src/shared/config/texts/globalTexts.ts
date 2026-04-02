f479512aec4f625997182fafe948d5de048f6f34
anemone-swg <maks-11-01-2003@mail.ru>
2026-04-02 20:07:16 +0700

    chore: add object for texts

---
+++ src/shared/config/texts/globalTexts.ts
@@ -0,0 +1,11 @@
+export const GLOBAL_TEXT = {
+  scenarios: "Сценарии",
+  create_scenario: "Создать новый сценарий",
+  no_scenarios: "Нет сценариев",
+  creating: "Создание...",
+  name: "Название",
+  updatedAt: "Дата обновления",
+  delete_scenario: "Удалить сценарий",
+  page_not_found: "Страница не найдена",
+  back_to_list: "Назад к списку",
+};
