81aa8e81d0557679f2b39e7db3a10cd001ae4501
anemone-swg <maks-11-01-2003@mail.ru>
2026-04-02 20:03:20 +0700

    style: new not found page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,5 +1,14 @@
+import { GLOBAL_TEXT } from "@/shared/config/texts/globalTexts.ts";
+
 const NotFoundPage = () => {
-  return <>404</>;
+  return (
+    <div className={"text-center"}>
+      <h1 className="mt-6 text-8xl font-bold">404</h1>
+      <h2 className="mt-6 text-2xl font-semibold">
+        {GLOBAL_TEXT.page_not_found}
+      </h2>
+    </div>
+  );
 };
 
 export default NotFoundPage;
