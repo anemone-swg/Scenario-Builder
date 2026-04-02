3cce41bd56109d4701a7af655ffd8f4a1340a364
anemone-swg <maks-11-01-2003@mail.ru>
2026-04-02 20:06:56 +0700

    fix: fix button's functionality

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -17,7 +17,9 @@ const Button = ({
     <button
       type="button"
       className={clsx(
-        "bg-button hover:bg-button-hover px-4 py-2 rounded flex items-center gap-2 transition-colors",
+        "bg-(--background-color-button) hover:bg-(--background-color-button-hover) px-4 py-2 rounded flex items-center gap-2 transition-all duration-200",
+        disabled &&
+          "opacity-50 cursor-not-allowed hover:bg-(--background-color-button)",
         className,
       )}
       disabled={disabled}
